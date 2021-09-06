409e8e43f8253fd3f79acae1953522d8c0496ed3
Aditya <adityamaurya5225@gmail.com>
2021-09-06 23:04:15 +0530

    changes

--- src/components/webTools/imageEditorOnline/ImageEditorOnline.jsx
+++ src/components/webTools/imageEditorOnline/ImageEditorOnline.jsx
@@ -4,42 +4,51 @@ import TuiImageEditor from "tui-image-editor";
 import "tui-image-editor/dist/tui-image-editor.css";
 import "tui-color-picker/dist/tui-color-picker.css";
 import FileSaver from 'file-saver';
+import { Button } from 'reactstrap';
+import { useSelector } from "react-redux";
+import { ImageEditorTheme } from './ImageEditorTheme';
 
 
 const ImageEditorOnline = () => {
 
-    const [imageEditorInst, setImageEditorInst] = useState(null);
+    const ytThumbnailData = useSelector(state => state.downloadYtThumbReducer.ytThumbnail);
 
     const editorBoxRef = useRef(null);
 
     useEffect(() => {
         let imageEditorInstData = new TuiImageEditor(editorBoxRef.current, {
             includeUI: {
+                loadImage: {
+                    path: ytThumbnailData?.data,
+                    name: "download",
+                },
                 // menu: ["shape", "filter", "text"],
-                initMenu: "filter",
+                // initMenu: "filter",
                 uiSize: {
                     width: "100%",
                     height: "625px"
                 },
-                menuBarPosition: "left"
+                menuBarPosition: "left",
+                theme: ImageEditorTheme,
             },
-            // cssMaxWidth: 700,
-            // cssMaxHeight: 500,
-            // selectionStyle: {
-            //     cornerSize: 20,
-            //     rotatingPointOffset: 70
-            // }
+            cssMaxWidth: 900,
+            // cssMaxHeight: 800,
+            selectionStyle: {
+                cornerSize: 20,
+                rotatingPointOffset: 70,
+            },
         });
 
-        setImageEditorInst(imageEditorInstData);
-    }, [])
+        console.log(imageEditorInstData)
 
-    console.log(imageEditorInst)
+    }, [ytThumbnailData])
+
 
 
     return (
-        <div>
-            <div ref={editorBoxRef}></div>
+        <div style={{ height: '100vh', backgroundColor: 'white' }}>
+
+            <div ref={editorBoxRef}> </div>
         </div>
     );
 }
--- src/components/webTools/youtubeThumbnailDownloader/YoutubeThumbnailDownloader.jsx
+++ src/components/webTools/youtubeThumbnailDownloader/YoutubeThumbnailDownloader.jsx
@@ -6,87 +6,112 @@ import {
     Col,
     Input,
     Button,
+    Spinner,
+    InputGroup,
+    InputGroupButtonDropdown,
+    DropdownItem,
+    DropdownMenu,
+    DropdownToggle,
 } from 'reactstrap';
-import { Field, reduxForm } from 'redux-form';
-import RenderInputField from '../../../containers/buildComponents/RenderInputField/RenderInputField';
 import { useSelector, useDispatch } from 'react-redux';
 import Icon from '@mdi/react'
 import { mdiDownload, mdiPencil } from '@mdi/js';
 import { fetchytThumbnailDataActions } from '../../../redux/actions/downloadYtThumbActions';
+import { Link } from 'react-router-dom';
+
 
 const YoutubeThumbnailDownloader = (props) => {
-    const { handleSubmit, initialize } = props
 
-    const getInputData = useSelector(state => state.form.YoutubeThumbnailDownloaderForm?.values);
     const ytThumbnailData = useSelector(state => state.downloadYtThumbReducer.ytThumbnail);
+    const ytThumbnailLoading = useSelector(state => state.downloadYtThumbReducer.loading);
 
     const [getImage, setGetImage] = useState(null);
-    const [imageSize, setImageSize] = useState('maxresdefault');
+    const [imageQuality, setImageQuality] = useState('Quality');
     const [getImageDimension, setGetImageDimension] = useState({});
+    const [inputValue, setInputValue] = useState('https://www.youtube.com/watch?v=9bZkp7q19f0');
+
+    const [dropdownOpen, setDropdownOpen] = useState(false);
+
+    const toggleDropDown = () => setDropdownOpen(!dropdownOpen);
 
     const dispatch = useDispatch();
 
     const imageRef = useRef(null);
-
-
-
-    // https://img.youtube.com/vi/<insert-youtube-video-id-here>/default.jpg
-    // https://img.youtube.com/vi/<insert-youtube-video-id-here>/mqdefault.jpg
-    // https://img.youtube.com/vi/<insert-youtube-video-id-here>/hqdefault.jpg
-    // https://img.youtube.com/vi/<insert-youtube-video-id-here>/sddefault.jpg
-    // https://img.youtube.com/vi/<insert-youtube-video-id-here>/maxresdefault.jpg
-
-
-    useEffect(() => {
-        initialize({ youtube_url: 'https://www.youtube.com/watch?v=9bZkp7q19f0' });
-    }, [])
-
+    const inputRef = useRef(null);
 
     useEffect(() => {
 
-        let results = getInputData?.youtube_url.match('[\\?&]v=([^&#]*)');
+        // inputRef.current.focus();
 
-        let videoUrl = (results == null) ? getInputData?.youtube_url : results[1];
+        let imageQlty = imageQuality == 'Quality' ? 'maxresdefault'
+            : imageQuality == 'High Quality' ? 'maxresdefault'
+                : imageQuality == 'Good Quality' ? 'sddefault'
+                    : imageQuality == 'Medium Quality' ? 'hqdefault'
+                        : imageQuality == 'Low Quality' ? 'mqdefault'
+                            : imageQuality == 'Default' ? 'default'
+                                : null;
 
-        let getImageUrl = `https://img.youtube.com/vi/${videoUrl}/${imageSize}.jpg`;
+        var regExp = /^.*(youtu.be\/|v\/|u\/\w\/|embed\/|watch\?v=|\&v=|\?v=)([^#\&\?]*).*/;
 
-        setGetImage(getImageUrl);
+        if (inputValue) {
 
-        dispatch(fetchytThumbnailDataActions({ image_url: getImageUrl }));
+            var match = inputValue.match(regExp);
 
-        imageRef.current.addEventListener("load", function () {
+            if (match && match[2].length == 11) {
 
-            if (imageSize == 'maxresdefault') {
-                setGetImageDimension({ height: this.naturalHeight, width: this.naturalWidth / 25 })
+                let results = inputValue.match('[\\?&]v=([^&#]*)');
+
+                let videoUrl = (results == null) ? inputValue : results[1];
+
+                let getImageUrl = `https://img.youtube.com/vi/${videoUrl}/${imageQlty}.jpg`;
+
+                dispatch(fetchytThumbnailDataActions({ image_url: getImageUrl }));
+
+                setGetImage(getImageUrl);
+
+                imageRef.current.addEventListener("load", function () {
+
+                    if (imageQuality == 'maxresdefault') {
+                        setGetImageDimension({ height: this.naturalHeight, width: this.naturalWidth / 25 })
+                    }
+                    else {
+                        setGetImageDimension({ height: this.naturalHeight, width: this.naturalWidth / 18 })
+                    }
+                });
             }
-            else {
-                setGetImageDimension({ height: this.naturalHeight, width: this.naturalWidth / 18 })
-            }
-        });
+        }
 
-    }, [getInputData]);
+    }, [inputValue, imageQuality]);
 
-
-    const submitData = data => {
-        // console.log(data);
-    }
 
     return (
         <Container fluid={true}>
             <Row>
                 <Col className='col-12'>
-                    <form className="form p-0 m-0 text-center py-4" onSubmit={handleSubmit(submitData)}>
+                    <form className="form p-0 m-0 text-center py-4" onSubmit={e => e.preventDefault()}>
                         <h2> Youtube Thumbnail Downloader </h2>
 
-                        <Field
-                            className='w-75 mx-auto text-center mt-4 mb-2'
-                            name="youtube_url"
-                            component={RenderInputField}
-                            type="text"
-                        />
+                        <InputGroup className='w-100 mx-auto'>
+                            <Input
+                                className='text-center'
+                                name='youtube_url'
+                                type='text'
+                                value={inputValue}
+                                onChange={e => setInputValue(e.target.value)}
+                            />
+                            <InputGroupButtonDropdown addonType="append" isOpen={dropdownOpen} toggle={toggleDropDown}>
+                                <DropdownToggle caret>  {imageQuality} </DropdownToggle>
+                                <DropdownMenu>
+                                    <DropdownItem onClick={() => setImageQuality('High Quality')}> High Quality </DropdownItem>
+                                    <DropdownItem onClick={() => setImageQuality('Good Quality')}> Good Quality </DropdownItem>
+                                    <DropdownItem onClick={() => setImageQuality('Medium Quality')}> Medium Quality </DropdownItem>
+                                    <DropdownItem onClick={() => setImageQuality('Low Quality')}> Low Quality </DropdownItem>
+                                    <DropdownItem onClick={() => setImageQuality('Default')}> Default </DropdownItem>
+                                </DropdownMenu>
+                            </InputGroupButtonDropdown>
+                        </InputGroup>
 
                         <div>
-
                             <div>
                                 <Button
                                     type='button'
@@ -94,16 +119,19 @@ const YoutubeThumbnailDownloader = (props) => {
                                     color='primary'
                                     href={ytThumbnailData?.data}
                                     download='YTThumbnail.png'
+                                    disabled={ytThumbnailLoading}
 
                                 >   <Icon path={mdiDownload}
                                     title="Download"
                                     size={1}
                                     /> <span> Download Image </span> </Button>
 
-                                <Button type='button' className='m-3' outline>  <Icon path={mdiPencil}
-                                    title="Download"
-                                    size={.9}
-                                />  <span> Edit Image </span> </Button>
+                                <Link to='/tools/image-editor-online' className={`${ytThumbnailLoading ? 'd-none' : 'd-initial'}`}>
+                                    <Button type='button' className='m-3' outline>  <Icon path={mdiPencil}
+                                        title="Download"
+                                        size={.9}
+                                    />  <span> Edit Image </span> </Button>
+                                </Link>
                             </div>
 
                             <img
@@ -111,7 +139,6 @@ const YoutubeThumbnailDownloader = (props) => {
                                 className='shadow border rounded pverflow-hidden'
                                 ref={imageRef}
                                 src={getImage}
-                                alt="Youtube Thumbnail"
                                 style={{ width: `${getImageDimension.width}%` }}
                             />
                         </div>
@@ -122,7 +149,4 @@ const YoutubeThumbnailDownloader = (props) => {
     );
 }
 
-export default reduxForm({
-    form: "YoutubeThumbnailDownloaderForm",
-})(YoutubeThumbnailDownloader);
-
+export default YoutubeThumbnailDownloader;
